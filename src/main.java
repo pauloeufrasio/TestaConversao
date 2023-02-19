66af171a53ad0a65819624d4f31aa2b79787aaf8
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-02-19 12:31:32 -0300

    Conversão

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        double salario = 1270.50;
+
+        int valor = (int) salario; // Forçando conversão = casting
+
+        System.out.println(valor); // Testando casting
+    }
+}

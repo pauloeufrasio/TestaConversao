9691fbea48166e776ac2ee4b5efd4c9d4948b265
paulo <henriquepaulo0606@gmail.com>
2023-03-22 12:42:55 -0300

    Testando casting

---
+++ src/TestaConversao.java
@@ -0,0 +1,11 @@
+public class TestaConversao {
+    public static void main(String[] args) {
+
+        //  Atribuindo valor com ponto flutuante
+        double salario = 1270.50;
+        // Forçando casting Pra Tornar valor double em inteiro, obs ira te retornar valor inteiro sem ponto flutuante
+        int valor = (int) salario;
+        System.out.println(valor);
+
+    }
+}
